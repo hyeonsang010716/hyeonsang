cbdf4d5d0ff88c5d3f0bf6a4672ae40f66ee0cf1
hyeonsang010716 <67507183+hyeonsang010716@users.noreply.github.com>
2020-12-21 21:27:01 +0900

    Add files via upload

---
+++ 동아리/알고리즘/1로 만들기.py
@@ -0,0 +1,18 @@
+x = int(input())
+dp = [0]*10001
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 1
+for i in range(6,x+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i],dp[i//2] + 1)
+    elif i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    elif i % 5 == 0:
+        dp[i] = min(dp[i], dp[i//5] + 1)
+
+print(dp[x])
